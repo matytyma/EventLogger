cecb5726bd251fc828701ab74f7c00e90caf4e33
matytyma <matytyma22@gmail.com>
2024-07-28 19:18:31 +0200

    Add `Player`, `Location` and `World` serializers

--- src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
@@ -1,5 +1,12 @@
 package dev.matytyma.eventlogger
 
-fun Any?.serialize() = when(this) {
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.entity.Player
+
+fun Any?.serialize(): String = when(this) {
+    is Player -> "Player(name=$name, id=$uniqueId)"
+    is Location -> "Location(world=${world.serialize()}, x=$x, y=$y, z=$z, yaw=$yaw, pitch=$pitch)"
+    is World -> "World(name=$name)"
     else -> this.toString()
 }

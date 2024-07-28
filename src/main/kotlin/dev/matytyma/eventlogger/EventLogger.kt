68914bdf959dec323921231b30574bc1b0ff7ae2
matytyma <matytyma22@gmail.com>
2024-07-28 19:12:46 +0200

    Load config

--- src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -1,14 +1,22 @@
 package dev.matytyma.eventlogger
 
+import org.bukkit.event.Event
 import org.bukkit.plugin.java.JavaPlugin
 
 class EventLogger : JavaPlugin() {
 
     override fun onEnable() {
-        // Plugin startup logic
-    }
+        saveDefaultConfig()
 
-    override fun onDisable() {
-        // Plugin shutdown logic
+        val eventList = mutableListOf<Class<out Event>>()
+
+        config.getStringList("events").forEach() {
+            try {
+                @Suppress("UNCHECKED_CAST")
+                eventList.add(Class.forName("org.bukkit.event.$it") as Class<out Event>)
+            } catch (exception: Exception) {
+                logger.warning("Event '$it' does not exist")
+            }
+        }
     }
 }

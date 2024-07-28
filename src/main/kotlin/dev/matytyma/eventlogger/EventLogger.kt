e988f75d53598722e4154e912d0a15e8b7479ef1
matytyma <matytyma22@gmail.com>
2024-07-28 19:45:14 +0200

    Add event data logging function

--- src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -2,9 +2,20 @@ package dev.matytyma.eventlogger
 
 import org.bukkit.event.Event
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.logging.Logger
+
+private lateinit var pluginLogger: Logger
+
+fun logEventData(event: Event, vararg data: Pair<String, Any?>) {
+    pluginLogger.info("[[ ${event.eventName} ]]")
+    data.forEach { (title, value) ->
+        pluginLogger.info("$title: ${value.serialize()}")
+    }
+}
 
 class EventLogger : JavaPlugin() {
     override fun onEnable() {
+        pluginLogger = logger
         saveDefaultConfig()
 
         val eventList = mutableListOf<Class<out Event>>()

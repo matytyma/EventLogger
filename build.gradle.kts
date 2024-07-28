389a173f2e350c1a3cd38e90176b1f72405cef53
matytyma <matytyma22@gmail.com>
2024-07-28 18:22:25 +0200

    Simplify build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,12 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://repo.papermc.io/repository/maven-public/") {
-        name = "papermc-repo"
-    }
-    maven("https://oss.sonatype.org/content/groups/public/") {
-        name = "sonatype"
-    }
+    maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("stdlib"))
 }
 
 val targetJavaVersion = 21

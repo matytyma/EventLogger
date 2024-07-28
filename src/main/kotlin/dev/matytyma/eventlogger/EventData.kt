88d2ac2b2b9aa14e618031f6cc91a2caaca84452
matytyma <matytyma22@gmail.com>
2024-07-28 21:19:08 +0200

    Add `eventData` property

--- src/main/kotlin/dev/matytyma/eventlogger/EventData.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventData.kt
@@ -1,5 +1,10 @@
 package dev.matytyma.eventlogger
 
 import org.bukkit.event.Event
+import org.bukkit.event.player.PlayerMoveEvent
 
 class EventData<T : Event>(val eventClass: Class<T>, val logFunction: (T) -> Unit)
+
+val eventData = setOf(
+    EventData(PlayerMoveEvent::class.java) { event -> println(event) }
+)

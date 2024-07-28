f0564bd374488f64e1509846b37eb6e22d8b1d13
matytyma <matytyma22@gmail.com>
2024-07-28 21:19:31 +0200

    Restructure, register events

--- src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -1,24 +1,23 @@
 package dev.matytyma.eventlogger
 
-import org.bukkit.event.Event
+import org.bukkit.event.*
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.logging.Logger
 
 private lateinit var pluginLogger: Logger
 
 fun logEventData(event: Event, vararg data: Pair<String, Any?>) {
-    pluginLogger.info("[[ ${event.eventName} ]]")
+    pluginLogger.info("[[ ${event.eventName}${if (event is Cancellable && event.isCancelled) " - cancelled" else ""} ]]")
     data.forEach { (title, value) ->
         pluginLogger.info("$title: ${value.serialize()}")
     }
 }
 
 class EventLogger : JavaPlugin() {
-    override fun onEnable() {
-        pluginLogger = logger
-        saveDefaultConfig()
+    private val eventList = mutableListOf<Class<out Event>>()
 
-        val eventList = mutableListOf<Class<out Event>>()
+    private fun loadConfig() {
+        saveDefaultConfig()
 
         config.getStringList("events").forEach {
             try {
@@ -28,5 +27,20 @@ class EventLogger : JavaPlugin() {
                 logger.warning("Event '$it' does not exist")
             }
         }
+    }
+
+    private fun registerEvents() {
+        val manager = server.pluginManager
+        val listener = object : Listener {}
+        eventList.forEach {
+            println("registering $listener, $it")
+            manager.registerEvent(it, listener, EventPriority.MONITOR, { _, event -> /* TODO: Find a suitable `EventData` instance*/ }, this)
+        }
+    }
+
+    override fun onEnable() {
+        pluginLogger = logger
+        loadConfig()
+        registerEvents()
     }
 }

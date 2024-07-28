355462bf5f95f1842be0c10f79bed67ae6549f4f
matytyma <matytyma22@gmail.com>
2024-07-28 19:40:55 +0200

    Formatting

--- src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -4,13 +4,12 @@ import org.bukkit.event.Event
 import org.bukkit.plugin.java.JavaPlugin
 
 class EventLogger : JavaPlugin() {
-
     override fun onEnable() {
         saveDefaultConfig()
 
         val eventList = mutableListOf<Class<out Event>>()
 
-        config.getStringList("events").forEach() {
+        config.getStringList("events").forEach {
             try {
                 @Suppress("UNCHECKED_CAST")
                 eventList.add(Class.forName("org.bukkit.event.$it") as Class<out Event>)

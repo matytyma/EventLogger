18aad0ff9acc42bbf5920df5a0b3a4f79f6c2d77
matytyma <matytyma22@gmail.com>
2024-07-28 16:21:24 +0200

    Initial commit

---
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -0,0 +1,14 @@
+package dev.matytyma.eventlogger
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class EventLogger : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}

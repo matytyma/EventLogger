b589ada732cc66cb0325efa3ff4445eafd378a32
matytyma <matytyma22@gmail.com>
2024-07-29 14:23:56 +0200

    `EventLogger` multiple changes
    - Switch to SLF4J logger
    - Implement calling the logging function
    - Accept just simple class names in config

--- src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventLogger.kt
@@ -2,37 +2,43 @@ package dev.matytyma.eventlogger
 
 import org.bukkit.event.*
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.logging.Logger
+import org.slf4j.Logger
 
-private lateinit var pluginLogger: Logger
+lateinit var plugin: EventLogger
 
 class EventLogger : JavaPlugin() {
-    private val eventList = mutableListOf<Class<out Event>>()
+    lateinit var logger: Logger
+    private val eventList = mutableListOf<EventData<*>>()
 
     private fun loadConfig() {
         saveDefaultConfig()
 
-        config.getStringList("events").forEach {
+        config.getStringList("events").forEach { event ->
             try {
-                @Suppress("UNCHECKED_CAST")
-                eventList.add(Class.forName("org.bukkit.event.$it") as Class<out Event>)
-            } catch (exception: Exception) {
-                logger.warning("Event '$it' does not exist")
+                eventList += eventData.first { it.eventClass.simpleName == event }
+            } catch (exception: NoSuchElementException) {
+                logger.warn("Logger for event '$event' does not exist")
             }
         }
+    }
+
+    private fun <T : Event> logEvent(eventData: EventData<T>, event: Event) {
+        if (!eventData.eventClass.isInstance(event)) return
+        @Suppress("UNCHECKED_CAST") eventData.logFunction(event as T)
     }
 
     private fun registerEvents() {
         val manager = server.pluginManager
         val listener = object : Listener {}
         eventList.forEach {
-            println("registering $listener, $it")
-            manager.registerEvent(it, listener, EventPriority.MONITOR, { _, event -> /* TODO: Find a suitable `EventData` instance*/ }, this)
+            val executor = { _: Listener, event: Event -> logEvent(it, event) }
+            manager.registerEvent(it.eventClass, listener, EventPriority.MONITOR, executor, this)
         }
     }
 
     override fun onEnable() {
-        pluginLogger = logger
+        logger = slF4JLogger
+        plugin = this
         loadConfig()
         registerEvents()
     }

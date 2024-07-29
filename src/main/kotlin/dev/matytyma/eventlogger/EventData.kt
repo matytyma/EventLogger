13e59361573893b68a7334f7619b431ce1879848
matytyma <matytyma22@gmail.com>
2024-07-29 14:58:41 +0200

    Change `logEventData` to and extension function

--- src/main/kotlin/dev/matytyma/eventlogger/EventData.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/EventData.kt
@@ -2,7 +2,6 @@ package dev.matytyma.eventlogger
 
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
-import org.bukkit.event.player.PlayerMoveEvent
 
 class EventData<T : Event>(val eventClass: Class<T>, val logFunction: (T) -> Unit)
 
@@ -10,8 +9,8 @@ val eventData = setOf(
     EventData(PlayerMoveEvent::class.java) { event -> println(event) }
 )
 
-fun logEventData(event: Event, vararg data: Pair<String, Any?>) {
-    plugin.logger.debug("[[ ${event.eventName}${if (event is Cancellable && event.isCancelled) " - cancelled" else ""} ]]")
+fun Event.logData(vararg data: Pair<String, Any?>) {
+    plugin.logger.info("[[ $eventName${if (this is Cancellable && isCancelled) " - cancelled" else ""} ]]")
     data.forEach { (title, value) ->
         plugin.logger.info("$title: ${value.serialize()}")
     }

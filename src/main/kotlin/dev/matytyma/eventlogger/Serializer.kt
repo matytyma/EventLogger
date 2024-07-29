b38f5559bc7da375e9c86dda05ad8f354e3197bb
matytyma <matytyma22@gmail.com>
2024-07-29 18:23:38 +0200

    Add `Block` and `Material` serializers

--- src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
@@ -1,12 +1,14 @@
 package dev.matytyma.eventlogger
 
-import org.bukkit.Location
-import org.bukkit.World
+import org.bukkit.*
+import org.bukkit.block.Block
 import org.bukkit.entity.Player
 
 fun Any?.serialize(): String = when(this) {
-    is Player -> "Player(name=$name, id=$uniqueId)"
+    is Block -> "Block(position=[$x, $y, $z], type=${type.serialize()}, data=$blockData)"
     is Location -> "Location(world=${world.serialize()}, x=$x, y=$y, z=$z, yaw=$yaw, pitch=$pitch)"
+    is Material -> "Material.$name"
+    is Player -> "Player(name=$name, id=$uniqueId)"
     is World -> "World(name=$name)"
     else -> this.toString()
 }

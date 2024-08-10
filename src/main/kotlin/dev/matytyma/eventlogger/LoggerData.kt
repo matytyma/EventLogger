ec15e4f5b752736fca87475dd25a8ad9ccbfcf0d
matytyma <matytyma22@gmail.com>
2024-08-10 14:08:48 +0200

    Add border around log entries

--- src/main/kotlin/dev/matytyma/eventlogger/LoggerData.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/LoggerData.kt
@@ -3,17 +3,26 @@ package dev.matytyma.eventlogger
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.block.*
+import kotlin.math.max
 
 class LoggerData<T : Event>(val eventClass: Class<T>, val properties: T.() -> List<Pair<String, Any?>>) {
     fun logData(event: Event) {
         if (!eventClass.isInstance(event)) return
         val eventProperties = loggerData.filter {
             it.eventClass.isInstance(event)
-        }.flatMap { it.getData(event) }
-        plugin.logger.info("[[ ${event.eventName}${if (event is Cancellable && event.isCancelled) " - cancelled" else ""} ]]")
+        }.flatMap { it.getData(event) }.map { it.first to it.second.serialize() }
+
+        val header = "${event.eventName}${if (event is Cancellable && event.isCancelled) " - cancelled" else ""}"
+        val width = max(header.length, eventProperties.maxOf {
+            (title, value) -> title.length + value.length
+        }) + 2
+
+        plugin.logger.info("┏${"━".repeat((width - header.length) / 2)} $header ${"━".repeat((width - header.length + 1) / 2)}┓")
         eventProperties.forEach { (title, value) ->
-            plugin.logger.info("$title: ${value.serialize()}")
+            val lineWidth = title.length + value.length + 2
+            plugin.logger.info("┃ $title: $value ${" ".repeat(width - lineWidth)}┃")
         }
+        plugin.logger.info("┗${"━".repeat(width + 2)}┛")
     }
 
     @Suppress("UNCHECKED_CAST")

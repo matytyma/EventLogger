c09214705670e6fe8e7019b6aeea1c53371faa82
matytyma <matytyma22@gmail.com>
2024-08-10 14:23:57 +0200

    Use custom unnamed logger for event logging

--- src/main/kotlin/dev/matytyma/eventlogger/LoggerData.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/LoggerData.kt
@@ -3,9 +3,14 @@ package dev.matytyma.eventlogger
 import org.bukkit.event.Cancellable
 import org.bukkit.event.Event
 import org.bukkit.event.block.*
+import org.slf4j.LoggerFactory
 import kotlin.math.max
 
 class LoggerData<T : Event>(val eventClass: Class<T>, val properties: T.() -> List<Pair<String, Any?>>) {
+    companion object {
+        private val logger = LoggerFactory.getLogger("")
+    }
+
     fun logData(event: Event) {
         if (!eventClass.isInstance(event)) return
         val eventProperties = loggerData.filter {
@@ -17,12 +22,12 @@ class LoggerData<T : Event>(val eventClass: Class<T>, val properties: T.() -> Li
             (title, value) -> title.length + value.length
         }) + 2
 
-        plugin.logger.info("┏${"━".repeat((width - header.length) / 2)} $header ${"━".repeat((width - header.length + 1) / 2)}┓")
+        logger.info("┏${"━".repeat((width - header.length) / 2)} $header ${"━".repeat((width - header.length + 1) / 2)}┓")
         eventProperties.forEach { (title, value) ->
             val lineWidth = title.length + value.length + 2
-            plugin.logger.info("┃ $title: $value ${" ".repeat(width - lineWidth)}┃")
+            logger.info("┃ $title: $value ${" ".repeat(width - lineWidth)}┃")
         }
-        plugin.logger.info("┗${"━".repeat(width + 2)}┛")
+        logger.info("┗${"━".repeat(width + 2)}┛")
     }
 
     @Suppress("UNCHECKED_CAST")

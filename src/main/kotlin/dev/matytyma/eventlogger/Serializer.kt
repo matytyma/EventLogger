7dbed9270975a4cbb7e194df91e1a8af3e8ba402
matytyma <matytyma22@gmail.com>
2024-08-11 16:43:06 +0200

    Create `formatValue` function

--- src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/Serializer.kt
@@ -12,3 +12,20 @@ fun Any?.serialize(): String = when (this) {
     is World -> "World(name=$name)"
     else -> this.toString()
 }
+private fun Any?.formatValue(): String = when (this) {
+    is Number -> toString()
+    is CharSequence -> toString()
+    is Boolean -> toString()
+    is Collection<*> -> joinToString(", ", "[", "]") { it.formatValue() }
+    is Array<*> -> joinToString(", ", "[", "]") { it.formatValue() }
+    is ByteArray -> joinToString(", ", "[", "]")
+    is CharArray -> joinToString(", ", "[", "]")
+    is ShortArray -> joinToString(", ", "[", "]")
+    is IntArray -> joinToString(", ", "[", "]")
+    is LongArray -> joinToString(", ", "[", "]")
+    is FloatArray -> joinToString(", ", "[", "]")
+    is DoubleArray -> joinToString(", ", "[", "]")
+    is BooleanArray -> joinToString(", ", "[", "]")
+    is Enum<*> -> "${javaClass.simpleName}.$name"
+    else -> serialize()
+}

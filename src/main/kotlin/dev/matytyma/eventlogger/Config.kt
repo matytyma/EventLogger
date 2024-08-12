ac6e95748806ecf415284f232d0d7742191e8ffd
matytyma <matytyma22@gmail.com>
2024-08-12 18:03:20 +0200

    Optimize imports

--- src/main/kotlin/dev/matytyma/eventlogger/Config.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/Config.kt
@@ -1,7 +1,6 @@
 package dev.matytyma.eventlogger
 
 import net.kyori.adventure.text.Component
-import org.bukkit.configuration.ConfigurationSection
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.properties.Delegates
@@ -89,4 +88,3 @@ object Config {
         bottomRightBorder = border?.getChar("bottom-right") ?: '┛'
     }
 }
-

7fdcadb37494f535c1c2e0a37171183653466d49
matytyma <matytyma22@gmail.com>
2024-08-13 12:27:12 +0200

    Multiple commands support - impl

--- src/main/kotlin/dev/matytyma/eventlogger/command/MainCommand.kt
+++ src/main/kotlin/dev/matytyma/eventlogger/command/MainCommand.kt
@@ -1,5 +1,29 @@
 package dev.matytyma.eventlogger.command
 
-import org.bukkit.command.TabExecutor
+import dev.matytyma.eventlogger.plugin
+import org.bukkit.command.*
 
-object MainCommand : TabExecutor
+object MainCommand : TabExecutor {
+    private val commands: Map<String, CommandExecutor>
+        get() = plugin.commands - "eventlogger"
+    private val completers: Map<String, TabCompleter>
+        get() = commands.filterValues { it is TabExecutor }.mapValues { it.value as TabCompleter }
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<String>,
+    ): Boolean = if (args.isEmpty()) false else {
+        commands[args[0]]?.onCommand(sender, command, label, args.sliceArray(1..<args.size)) ?: false
+    }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<String>,
+    ): List<String> = if (args.size == 1) {
+        commands.keys.map { it.removePrefix("eventlogger") }.filter { it.startsWith(args[0]) }
+    } else completers[args[0]]?.onTabComplete(sender, command, label, args) ?: emptyList()
+}

6b01b4a6d568fb3ee23b2303fe735b254451bd76
matytyma <matytyma22@gmail.com>
2024-08-13 15:29:19 +0200

    Fix subcommand execution

--- src/main/kotlin/command/MainCommand.kt
+++ src/main/kotlin/command/MainCommand.kt
@@ -3,9 +3,12 @@ package dev.matytyma.eventlogger.command
 import dev.matytyma.eventlogger.plugin
 import org.bukkit.command.*
 
+
 object MainCommand : TabExecutor {
+    private const val COMMAND_NAME = "eventlogger"
+
     private val commands: Map<String, CommandExecutor>
-        get() = plugin.commands - "eventlogger"
+        get() = plugin.commands - COMMAND_NAME
     private val completers: Map<String, TabCompleter>
         get() = commands.filterValues { it is TabExecutor }.mapValues { it.value as TabCompleter }
 
@@ -15,7 +18,7 @@ object MainCommand : TabExecutor {
         label: String,
         args: Array<String>,
     ): Boolean = if (args.isEmpty()) false else {
-        commands[args[0]]?.onCommand(sender, command, label, args.sliceArray(1..<args.size)) ?: false
+        commands["$COMMAND_NAME${args[0]}"]?.onCommand(sender, command, label, args.sliceArray(1..<args.size)) ?: false
     }
 
     override fun onTabComplete(
@@ -24,6 +27,6 @@ object MainCommand : TabExecutor {
         label: String,
         args: Array<String>,
     ): List<String> = if (args.size == 1) {
-        commands.keys.map { it.removePrefix("eventlogger") }.filter { it.startsWith(args[0]) }
+        commands.keys.map { it.removePrefix(COMMAND_NAME) }.filter { it.startsWith(args[0]) }
     } else completers[args[0]]?.onTabComplete(sender, command, label, args) ?: emptyList()
 }

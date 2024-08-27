e7fc42b6efadb83c92ed5490203ea616a48ba7de
matytyma <matytyma22@gmail.com>
2024-08-27 17:12:55 +0200

    Store listeners for registered events

--- src/main/kotlin/EventLogger.kt
+++ src/main/kotlin/EventLogger.kt
@@ -6,25 +6,30 @@ import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.TabExecutor
 import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
 
 lateinit var plugin: EventLogger
 
 val mm: MiniMessage = MiniMessage.miniMessage()
 
+var events: Set<LoggerData<*>> = emptySet()
+
 class EventLogger : JavaPlugin() {
+    val listeners: MutableMap<LoggerData<*>, Listener> = mutableMapOf()
+
     val commands: Map<String, CommandExecutor> = mapOf(
         "reload" to ReloadCommand,
         "whitelist" to WhitelistCommand,
     )
 
-    fun registerEvents() {
-        Config.events.forEach {
-            plugin.on(it.eventClass, EventPriority.MONITOR) {
-                it.logData(this)
-            }
+    fun registerEvent(logger: LoggerData<*>) {
+        listeners[logger] = plugin.on(logger.eventClass, EventPriority.MONITOR) {
+            logger.logData(this)
         }
     }
+
+    fun registerEvents() = events.forEach(::registerEvent)
 
     private fun registerCommands() {
         getCommand("eventlogger")?.apply {

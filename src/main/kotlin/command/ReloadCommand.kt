0e14df5416b856a389add2d00bfb3cf3b96fad04
matytyma <matytyma22@gmail.com>
2024-08-29 11:58:35 +0200

    Remove default completion for reload command

--- src/main/kotlin/command/ReloadCommand.kt
+++ src/main/kotlin/command/ReloadCommand.kt
@@ -4,7 +4,7 @@ import dev.matytyma.eventlogger.*
 import org.bukkit.command.*
 import org.bukkit.event.HandlerList
 
-object ReloadCommand : CommandExecutor {
+object ReloadCommand : TabExecutor {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
         Config.loadConfig()
         HandlerList.unregisterAll(plugin)
@@ -12,4 +12,11 @@ object ReloadCommand : CommandExecutor {
         sender.sendPrefixedMessage("Reload complete.")
         return true
     }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<String>,
+    ): List<String> = emptyList()
 }

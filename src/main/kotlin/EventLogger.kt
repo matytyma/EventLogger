1252861095592cf4f370f110d4658234a7967934
matytyma <matytyma22@gmail.com>
2024-08-31 15:02:14 +0200

    Rewrite configuration in Toml (#2)

--- src/main/kotlin/EventLogger.kt
+++ src/main/kotlin/EventLogger.kt
@@ -1,11 +1,10 @@
 package dev.matytyma.eventlogger
 
-import com.akuleshov7.ktoml.Toml
 import com.akuleshov7.ktoml.file.TomlFileReader
 import com.akuleshov7.ktoml.file.TomlFileWriter
 import dev.matytyma.eventlogger.command.MainCommand
+import dev.matytyma.eventlogger.config.Config
 import dev.matytyma.minekraft.plugin.on
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.serializer
 import net.kyori.adventure.text.minimessage.MiniMessage
 import org.bukkit.event.EventPriority
@@ -22,17 +21,22 @@ val mm: MiniMessage = MiniMessage.miniMessage()
 var events: Set<LoggerData<*>> = emptySet()
 
 class EventLogger : JavaPlugin() {
-    lateinit var config: Config
-    val listeners: MutableMap<LoggerData<*>, Listener> = mutableMapOf()
+    private val listeners: MutableMap<LoggerData<*>, Listener> = mutableMapOf()
 
     fun loadConfig() {
+        config
         val file = File(dataFolder, "config.toml")
         if (file.exists()) {
             cfg = TomlFileReader.decodeFromFile(serializer(), file.absolutePath)
         } else {
             cfg = Config()
-            TomlFileWriter().encodeToFile(serializer(), cfg, file.absolutePath)
+            saveConfig()
         }
+    }
+
+    override fun saveConfig() {
+        val filePath = File(dataFolder.also(File::mkdirs), "config.toml").absolutePath
+        TomlFileWriter().encodeToFile(serializer(), cfg, filePath)
     }
 
     fun registerEvent(logger: LoggerData<*>) {
